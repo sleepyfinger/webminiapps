1b4fdd8f4c1662dc07637820fc9e1595dc7cf7c7
hebory <hebory@gmail.com>
2025-02-22 23:56:53 +0900

    TLP PWA 적용

---
+++ tenlinepuzzle/service-worker.js
@@ -0,0 +1,23 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open("app-cache")
+      .then((cache) =>
+        cache.addAll([
+          "index.html",
+          "game.js",
+          "get_score.mp3",
+          "RiaSans-ExtraBold.otf",
+          "icon_naver_blog.png",
+        ])
+      )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
