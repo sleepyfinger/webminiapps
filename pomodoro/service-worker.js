043e019978ea37eca79d2be5d5ecef472968f935
hebory <hebory@gmail.com>
2025-02-22 18:23:58 +0900

    PWA 테스트

---
+++ pomodoro/service-worker.js
@@ -0,0 +1,17 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open("app-cache")
+      .then((cache) =>
+        cache.addAll(["/index.html", "/style.css", "/script.js"])
+      )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
