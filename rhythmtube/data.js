f5f62274f5754f60f05a7f74b9009331e065380d
hebory <hebory@gmail.com>
2025-03-12 15:15:08 +0900

    음악 목록 추가

--- rhythmtube/data.js
+++ rhythmtube/data.js
@@ -8,6 +8,279 @@ const songData = [
   {
     url: "https://youtu.be/sgiUkiSwh9Q",
   },
+  {
+    url: "https://youtu.be/jy0cJllOV1c",
+  },
+  {
+    url: "https://youtu.be/1giyGtBTw3Y",
+  },
+  {
+    url: "https://youtu.be/6GQV6lhwgNs",
+  },
+  {
+    url: "https://youtu.be/bD8y_47cbTs",
+  },
+  {
+    url: "https://youtu.be/7Q61ZtM2qyM",
+  },
+  {
+    url: "https://youtu.be/K8WC6uWyC9I",
+  },
+  {
+    url: "https://youtu.be/Q6puNlv3Cm4",
+  },
+  {
+    url: "https://youtu.be/i4SRH9jfUMQ",
+  },
+  {
+    url: "https://youtu.be/oRg-7iku6Ok",
+  },
+  {
+    url: "https://youtu.be/TsPgxPgp84o",
+  },
+  {
+    url: "https://youtu.be/BgZH3GRYG-k",
+  },
+  {
+    url: "https://youtu.be/QTsZ9n0VW6U",
+  },
+  {
+    url: "https://youtu.be/1PST1BsjjMo",
+  },
+  {
+    url: "https://youtu.be/NVYCfOQGOdM",
+  },
+  {
+    url: "https://youtu.be/T84CDxlwsXw",
+  },
+  {
+    url: "https://youtu.be/yAiEUKq7qhk",
+  },
+  {
+    url: "https://youtu.be/50qIiJTpUQs",
+  },
+  {
+    url: "https://youtu.be/w29E_TCtotc",
+  },
+  {
+    url: "https://youtu.be/BQif6mSCl8Y",
+  },
+  {
+    url: "https://youtu.be/_OsnBqrh6yg",
+  },
+  {
+    url: "https://youtu.be/OrFyzG5yTC4",
+  },
+  {
+    url: "https://youtu.be/JY-gJkMuJ94",
+  },
+  {
+    url: "https://youtu.be/2cw9Ef2pjk8",
+  },
+  {
+    url: "https://youtu.be/uw-ZnT-iAEI",
+  },
+  {
+    url: "https://youtu.be/s_91Rtt1iqU",
+  },
+  {
+    url: "https://youtu.be/IhIDFiYWMbc",
+  },
+  {
+    url: "https://youtu.be/wyhwJnNpVJI",
+  },
+  {
+    url: "https://youtu.be/fJiRofpQ_B8",
+  },
+  {
+    url: "https://youtu.be/fgSXAKsq-Vo",
+  },
+  {
+    url: "https://youtu.be/YmELthNomns",
+  },
+  {
+    url: "https://youtu.be/4iKrtoiUpqU",
+  },
+  {
+    url: "https://youtu.be/8KTFf2X-ago",
+  },
+  {
+    url: "https://youtu.be/PxquPfg6fVk",
+  },
+  {
+    url: "https://youtu.be/YZ0xA3LUzNQ",
+  },
+  {
+    url: "https://youtu.be/S87M2j33Zdw",
+  },
+  {
+    url: "https://youtu.be/Q2Cc8XoqMpM",
+  },
+  {
+    url: "https://youtu.be/PZOlV-1h7zA",
+  },
+  {
+    url: "https://youtu.be/Brf3LWwNVTk",
+  },
+  {
+    url: "https://youtu.be/KLOevIVg7Ww",
+  },
+  {
+    url: "https://youtu.be/VdLWs5Wi5B4",
+  },
+  {
+    url: "https://youtu.be/7sFIjj5BW3M",
+  },
+  {
+    url: "https://youtu.be/8XMdOV37ICw",
+  },
+  {
+    url: "https://youtu.be/cApabFGK8VA",
+  },
+  {
+    url: "https://youtu.be/Empfi8q0aas",
+  },
+  {
+    url: "https://youtu.be/_T4ck9kc1ws",
+  },
+  {
+    url: "https://youtu.be/l8e1Byk1Dx0",
+  },
+  {
+    url: "https://youtu.be/SXE-gIU3yJs",
+  },
+  {
+    url: "https://youtu.be/4xZYZTWH0qk",
+  },
+  {
+    url: "https://youtu.be/YqiMXmcS2Qo",
+  },
+  {
+    url: "https://youtu.be/T1VrwBpR7hY",
+  },
+  {
+    url: "https://youtu.be/T-LRqnvEi9E",
+  },
+  {
+    url: "https://youtu.be/UMIP5k1QvW8",
+  },
+  {
+    url: "https://youtu.be/8UVJAUVjhbw",
+  },
+  {
+    url: "https://youtu.be/aUejBogMCN4",
+  },
+  {
+    url: "https://youtu.be/niyDgrz7W_c",
+  },
+  {
+    url: "https://youtu.be/gG6LI0VADBQ",
+  },
+  {
+    url: "https://youtu.be/MRI2Ov5xtr4",
+  },
+  {
+    url: "https://youtu.be/7IcafhbXprU",
+  },
+  {
+    url: "https://youtu.be/5vmw67bRt_E",
+  },
+  {
+    url: "https://youtu.be/kra0f71EIgc",
+  },
+  {
+    url: "https://youtu.be/q8iiUYJsOUM",
+  },
+  {
+    url: "https://youtu.be/yZp6TI_S9D8",
+  },
+  {
+    url: "https://youtu.be/P0ORUA-VwWc",
+  },
+  {
+    url: "https://youtu.be/HnpChLQKIUQ",
+  },
+  {
+    url: "https://youtu.be/zwg81rjVf_0",
+  },
+  {
+    url: "https://youtu.be/O9uLXr86ag4",
+  },
+  {
+    url: "https://youtu.be/N0yaNNbSAf8",
+  },
+  {
+    url: "https://youtu.be/PhObK4-gdcM",
+  },
+  {
+    url: "https://youtu.be/NVns4yRoTlU",
+  },
+  {
+    url: "https://youtu.be/8qGfUWchv50",
+  },
+  {
+    url: "https://youtu.be/VE4sUxm2N7k",
+  },
+  {
+    url: "https://youtu.be/Cui5BkZ8Qcc",
+  },
+  {
+    url: "https://youtu.be/nWJB1U7cQVs",
+  },
+  {
+    url: "https://youtu.be/cvplKgW8qR0",
+  },
+  {
+    url: "https://youtu.be/Qu4OLof-TJI",
+  },
+  {
+    url: "https://youtu.be/aaqIHn61OVE",
+  },
+  {
+    url: "https://youtu.be/kBB-7pckyN4",
+  },
+  {
+    url: "https://youtu.be/JwEEIHzZNuQ",
+  },
+  {
+    url: "https://youtu.be/6hEvgKL0ClA",
+  },
+  {
+    url: "https://youtu.be/ufycyFgYPdc",
+  },
+  {
+    url: "https://youtu.be/oObEJ9CxbDk",
+  },
+  {
+    url: "https://youtu.be/oOtAyQQY4cc",
+  },
+  {
+    url: "https://youtu.be/buJjUHSalVA",
+  },
+  {
+    url: "https://youtu.be/55brpxSTMwg",
+  },
+  {
+    url: "https://youtu.be/TQqu1ZsvVFo",
+  },
+  {
+    url: "https://youtu.be/eVLI_YUSScg",
+  },
+  {
+    url: "https://youtu.be/fpewVDxGCaM",
+  },
+  {
+    url: "https://youtu.be/sVydSseSOCQ",
+  },
+  {
+    url: "https://youtu.be/7XFJ4s0j9z0",
+  },
+  {
+    url: "https://youtu.be/J-sdGqbwayc",
+  },
+  {
+    url: "https://youtu.be/owlLg3R9u6c",
+  },
 ];
 
 if (typeof dataLoaded === "function") {
