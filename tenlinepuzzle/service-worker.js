7572fb62b86ceb766dc0665bc137c3dae056ef70
hebory <hebory@gmail.com>
2025-04-15 09:25:10 +0900

    캐싱 전략 수정

--- tenlinepuzzle/service-worker.js
+++ tenlinepuzzle/service-worker.js
@@ -5,18 +5,22 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE_URLS))
   );
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((cacheNames) =>
-      Promise.all(
-        cacheNames
-          .filter((name) => name !== CACHE_NAME)
-          .map((name) => caches.delete(name))
+    caches
+      .keys()
+      .then((cacheNames) =>
+        Promise.all(
+          cacheNames
+            .filter((name) => name !== CACHE_NAME)
+            .map((name) => caches.delete(name))
+        )
       )
-    )
   );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
@@ -25,4 +29,10 @@ self.addEventListener("fetch", (event) => {
       .match(event.request)
       .then((response) => response || fetch(event.request))
   );
+});
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.action === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
 });
