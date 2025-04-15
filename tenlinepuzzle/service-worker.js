9fbd91fe84b48bd712bce1ea86f2f27fae8f317e
hebory <hebory@gmail.com>
2025-04-15 09:13:29 +0900

    캐시 문제 수정

--- tenlinepuzzle/service-worker.js
+++ tenlinepuzzle/service-worker.js
@@ -1,14 +1,21 @@
+const CACHE_NAME = "10linepuzzle-v1";
+const PRECACHE_URLS = ["index.html", "game.js", "get_score.mp3"];
+
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches
-      .open("app-cache")
-      .then((cache) =>
-        cache.addAll([
-          "index.html",
-          "game.js",
-          "get_score.mp3",
-        ])
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE_URLS))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
       )
+    )
   );
 });
 
